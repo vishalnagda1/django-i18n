232f1abade018ae027e21e3097cb35e45468424a
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 13:26:13 +0530

    create a post model in the core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('-created',)
+    
+    def __str__(self):
+        return self.title
+
