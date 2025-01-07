b351667011b5850e00050d804d60287fdd73784e
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 17:57:49 +0530

    add category model to admin script of core app

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
-from .models import Post
 from parler.admin import TranslatableAdmin
 
+from .models import Category, Post
+
 admin.site.register(Post, TranslatableAdmin)
+admin.site.register(Category, TranslatableAdmin)
