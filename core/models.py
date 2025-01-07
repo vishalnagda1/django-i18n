fa91340573b6fe78292f8fde86c66bc17f2c2db1
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 16:06:22 +0530

    update the core app model to support translation

--- core/models.py
+++ core/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    
+    title = models.CharField(_("title"), max_length=200)
+    content = models.TextField(_("content"))
+
     created = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
-        ordering = ('-created',)
-    
+        ordering = ("-created",)
+
     def __str__(self):
         return self.title
-
