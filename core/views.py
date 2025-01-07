7a8e10e4eed9e40e8e46c6595061a980e6a70595
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 19:49:52 +0530

    add flash message in view update the index template to display flash message

--- core/views.py
+++ core/views.py
@@ -1,9 +1,16 @@
+from django.contrib import messages
 from django.shortcuts import render
 
 from .models import Post
 
 
 def index(request):
+    if request.user.is_authenticated:
+        messages.success(request, "Hi, welcome!", extra_tags="alert alert-success")
+    else:
+        messages.warning(
+            request, "You are not authenticated!", extra_tags="alert alert-danger"
+        )
     posts = Post.objects.all()
 
     context = {"posts": posts}
