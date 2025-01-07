5e59558f9305a5d85c0ba514ae8efd23029b56d5
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 17:18:05 +0530

    update Post model to make it translatable model using parler

--- core/models.py
+++ core/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from parler.models import TranslatableModel, TranslatedFields
 
 
-class Post(models.Model):
-    title = models.CharField(_("title"), max_length=200)
-    content = models.TextField(_("content"))
+class Post(TranslatableModel):
+    translations = TranslatedFields(
+        title=models.CharField(_("title"), max_length=200),
+        content=models.TextField(_("content")),
+    )
 
     created = models.DateTimeField(auto_now_add=True)
 
