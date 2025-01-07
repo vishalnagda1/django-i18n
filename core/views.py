bb9a414fbc643529344c30dfc5d19a02adb3492c
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 19:58:12 +0530

    update the view to translate flash messages

--- core/views.py
+++ core/views.py
@@ -1,15 +1,15 @@
 from django.contrib import messages
 from django.shortcuts import render
-
+from django.utils.translation import gettext_lazy as _
 from .models import Post
 
 
 def index(request):
     if request.user.is_authenticated:
-        messages.success(request, "Hi, welcome!", extra_tags="alert alert-success")
+        messages.success(request, _("login-auth-message"), extra_tags="alert alert-success")
     else:
         messages.warning(
-            request, "You are not authenticated!", extra_tags="alert alert-danger"
+            request, _("logout-auth-message"), extra_tags="alert alert-danger"
         )
     posts = Post.objects.all()
 
