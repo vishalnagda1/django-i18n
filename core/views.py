61d76dd2d7806c5c3daed347a1c09ab3f569ba88
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 13:40:52 +0530

    update the index view of core to call the post model and it's data and pass it in context of index template

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .models import Post
+
+
 def index(request):
-    context = {}
+    posts = Post.objects.all()
+
+    context = {"posts": posts}
     return render(request, "index.html", context)
