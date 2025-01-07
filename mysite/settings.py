b6272da1a89b76488f2e919c3dc324abe542b6f3
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 14:51:10 +0530

    import gettext package in settings make language code to just en add language variable for supported languages

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,7 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en"
+
+LANGUAGES = [
+    ("en", _("English")),
+    ("hi", _("Hindi")),
+]
 
 TIME_ZONE = "UTC"
 
