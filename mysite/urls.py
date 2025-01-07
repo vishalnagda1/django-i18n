24f834f17b9bf222bc8fc97cdd3029923ea5baf2
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-07 15:48:41 +0530

    import i18n patterns and add rosetta path in the main urls file

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
+urlpatterns = i18n_patterns(
+    path("admin/", admin.site.urls),
+    path("rosetta/", include("rosetta.urls")),
+    path("", include("core.urls")),
+)
