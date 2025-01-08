578128f67460dd7a0b9310865c7576576e00b894
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-01-08 13:07:34 +0530

    fix the language change redirection issue

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
+from django.views.i18n import set_language
 
 urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("rosetta/", include("rosetta.urls")),
     path("", include("core.urls")),
+    path("i18n/setlang/", set_language, name="set_language"),
 )
